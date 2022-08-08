789ded78a72c3574aa15fff945c1133e2e1bebb1
Денис Шестернин <dshesternin@gmail.com>
2022-08-08 22:29:22 +0300

    Commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+         if (cost >= 20) {
+             cost = cost / 20;
+         } else {
+             cost = 0;
+         }
+         return cost;
+    }
+}
